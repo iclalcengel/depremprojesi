601900c5afd41efa43917e4ad4e56d893aab48bd
iclalcengel <115634785+iclalcengel@users.noreply.github.com>
2022-11-14 00:34:25 +0300

    Add files via upload
    arayuz.h kutuphanesi guncel hali

--- arayuz.h
+++ arayuz.h
@@ -1,22 +1,23 @@
 #pragma once
 #include <stdio.h>
 
-int arayuz_menu()      
+int arayuz_menu()   //arayuz ve menu fonksiyonu
 
 {
-    int secim=0;
+    int secim=0; //secim yapilirken integer deger tutmasi icin olusturulmus degisken
+    
     printf("***************************************************\n");
-    printf("*****TURKIYE DEPREM RAPOR VE SORGULAMA SISTEMI*****\n"); 
+    printf("*****TURKIYE DEPREM RAPOR VE SORGULAMA SISTEMI*****\n");    //sisteme hosgeldiniz mesaji
     printf("***************************************************\n");
 
-    printf("SORGULAMA ICIN = 1\n");
-    printf("SIRALAMA ICIN = 2\n"); 
-    printf("KAYDETME ICIN = 3\n");
-    printf("AYRISTIRMA ICIN = 4\n");
-    printf("ANALIZ ICIN = 5\n");
-    printf("CIKIS ICIN = 6\n");
-    printf("SECIMINIZI YAPINIZ-->");
-    scanf("%d",&secim);
+    printf("SORGULAMA ICIN = 1\n");    //menu islevleri
+    printf("SIRALAMA ICIN = 2\n");     //menu islevleri
+    printf("KAYDETME ICIN = 3\n");     //menu islevleri
+    printf("AYRISTIRMA ICIN = 4\n");   //menu islevleri
+    printf("ANALIZ ICIN = 5\n");       //menu islevleri
+    printf("CIKIS ICIN = 6\n");        //menu islevleri
+    printf("SECIMINIZI YAPINIZ-->");   //menu islevleri
+    scanf("%d",&secim);                //secimimizi girdigimiz integer degeri okuma 
 
-    return secim;
+    return secim;                      //fonksiyonun secilen menu islev degerini dondurmesi
 }
